e9a7996a8d61354a3a9a54dd283ea458ac4220a6
toBeOfUse <mitchjacov@gmail.com>
2022-07-14 14:03:09 -0400

    bake image dimensions into html

--- main.py
+++ main.py
@@ -20,25 +20,30 @@ if __name__ == "__main__":
         print(f"running make_gallery at {datetime.now().astimezone(eastern)}")
         puzzle = await SpellingBee.fetch_from_nyt()
         renderers = BeeRenderer.get_available_renderer_names()
-        filenames = []
+        html = []
         print(f"found {len(renderers)} renderers")
         for renderer in renderers:
             image = await BeeRenderer.get_renderer(renderer).render(puzzle, output_width=800)
             filename = uuid.uuid4().hex
-            filenames.append(filename)
             with open(images_output/(filename+".png"), "wb") as image_file:
                 image_file.write(image)
-            Image.open(BytesIO(image)).save(images_output/(filename+".webp"))
+            pil_image = Image.open(BytesIO(image))
+            pil_image.save(images_output/(filename+".webp"))
+            html.append(f"""<picture>
+                        <source srcset="images/{filename}.webp" type="image/webp">
+                        <img
+                            src="images/{filename}.png"
+                            loading="lazy"
+                            width="{pil_image.width}"
+                            height="{pil_image.height}"
+                        >
+                        </picture>""")
         print("done with images")
         with open(Path(__file__).parent / "index_template.html", "r") as template_file:
             template = template_file.read()
         with open(output_path/"index.html", "w+") as index_file:
             index = template.replace(
-                "{{gallery}}", "".join([
-                    f"""<picture>
-                        <source srcset="images/{f}.webp" type="image/webp">
-                        <img src="images/{f}.png" loading="lazy">
-                        </picture>""" for f in filenames])
+                "{{gallery}}", "".join(html)
             )
             index_file.write(index)
         print("done with index page")
