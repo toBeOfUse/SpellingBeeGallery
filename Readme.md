cbcc67a7840e7fb28b7c22d8829cf5f80735a16c
toBeOfUse <mitchjacov@gmail.com>
2022-07-14 12:57:14 -0400

    it's all here, folks

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Stupid-simple gallery for my various visualizations of the New York Times Spelling Bee. Updated daily [here](https://mitch.website/spellingbeegallery/).
---
+++ main.py
@@ -0,0 +1,33 @@
+import asyncio
+import aiocron
+import pytz
+from bee_engine import SpellingBee, BeeRenderer
+import uuid
+import sys
+from pathlib import Path
+
+if __name__ == "__main__":
+    output_path = Path(sys.argv[1])
+    images_output = output_path/"images"
+    if not images_output.exists(): images_output.mkdir()
+    @aiocron.crontab("5 3 * * *", pytz.timezone("US/Eastern"))
+    async def make_gallery():
+        puzzle = await SpellingBee.fetch_from_nyt()
+        renderers = BeeRenderer.get_available_renderer_names()
+        filenames = []
+        for renderer in renderers:
+            image = await BeeRenderer.get_renderer(renderer).render(puzzle, output_width=800)
+            filename = uuid.uuid4().hex+".png"
+            filenames.append(filename)
+            with open(images_output/filename, "wb") as image_file:
+                image_file.write(image)
+        with open(Path(__file__).parent / "index_template.html", "r") as template_file:
+            template = template_file.read()
+        with open(output_path/"index.html", "w+") as index_file:
+            index = template.replace(
+                "{{gallery}}", "".join([f"<img src=\"images/{f}\" />" for f in filenames])
+            )
+            index_file.write(index)
+    if output_path == Path("./test/"):
+        asyncio.run(make_gallery.func())
+
